11fcc3d13b70b84842f499fac4ed38ce0b17922a
spasm <maksimlushchan@gmail.com>
2023-11-04 15:46:39 +0200

    validation branch

--- js/finePayments.js
+++ js/finePayments.js
@@ -31,8 +31,50 @@ alert "Номер не співпадає" або "Сума не співпад�
 Якщо валідація проходить успішно, то виконати оплату,
  тобто вам потрібно видалити обєкт з DB
  */
-buttonSubmit.addEventListener('click',payFine);
+
+
 function payFine(){
-    //Звертаючись до властивості finesData ви отримуєте всі дані з файлу data.js
-    console.log(data.finesData)
+    const validation = [
+    validateFineNumber(fineNumber.value), 
+    validatePassport(passport.value), 
+    validateCreditCardNumber(creditCardNumber.value), 
+    validateCvv(cvv.value), 
+    validateAmount(amount.value)
+    ]
+    // Звертаючись до властивості finesData ви отримуєте всі дані з файлу data.js
+    if (validation.every(result => result === true)){
+        console.log(data.finesData);
+    }
 }
+
+function validateFineNumber(fineNumber){
+    for (let i of data.finesData) {
+        if (i["номер"] === fineNumber){
+            result = true;
+        }
+    }
+    alert("Номер не співпадає!");
+    return false;
+}
+
+function validatePassport(passport){
+    const re = /^[А-Я]{2}\d{6}$/;
+    return (re.test(passport)) ? true : alert("Не вірний паспортний номер"); false;
+}
+
+function validateCreditCardNumber(creditCardNumber){
+    const re = /^\d{16}$/;
+    return (re.test(creditCardNumber)) ? true : alert("Не вірна кредитна картка"); false;
+}
+
+function validateCvv(cvv){
+    const re = /^\d{3}$/;
+    return (re.test(cvv)) ? true : alert("Не вірний cvv"); false;
+}
+
+function validateAmount(amount){
+    const re = /^\d+$/;
+    return (re.test(amount)) ? true : alert("Не вірна сума"); false;
+}
+
+buttonSubmit.addEventListener('click',payFine);
